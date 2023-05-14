2e665debfa02889bc1d0381a0e0076d92fbc6908
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-14 13:45:07 +0800

    Test(integration): empty string pattern

--- grrs/tests/cli.rs
+++ grrs/tests/cli.rs
@@ -28,3 +28,18 @@ fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+#[test]
+fn pattern_is_empty_string() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    let file_content = "A test\nActual content\nMore content\nAnother test";
+    file.write_str(file_content)?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(file_content));
+
+    Ok(())
+}
