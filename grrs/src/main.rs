4c4daed007126ed2429183008f6c5516d725e2f6
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-05-14 09:06:41 +0800

    Working mvp

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use std::fs;
+
+#[derive(Debug, Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    let content = fs::read_to_string(&args.path).expect("Could not read file!");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
